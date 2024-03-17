9b3aeadf9358e83e58ef6dcc466e1b6b8dad0bab
ISILVER001 <isilver001@vplincsci052.ad.csbsju.edu>
2024-03-17 17:12:53 -0500

    editLastNameTest

---
+++ test/regression/editLastNameTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package regression;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cmc.backend.AccountController;
+import cmc.backend.DatabaseController;
+import cmc.backend.User;
+import cmc.frontend.UserInteraction;
+import junit.framework.Assert;
+/**
+ * 
+ */
+public class editLastNameTest {
+			private static String userToTest = "tester867475937583947375984";
+			private static String passToTest = "pass98754214678743807311459";
+			
+			@Before
+			public void setUp() throws Exception {
+				User user = new User(userToTest, passToTest, 'u', "k", "dt", 'Y');
+				DatabaseController.addUser(user);
+			}
+			
+
+			@After
+			public void tearDown() throws Exception {
+				DatabaseController.removeUser(userToTest);
+			}
+
+			@Test
+			public void test() {
+				String newLastName = "S";
+				AccountController.editLastName(userToTest, newLastName);
+				User user = DatabaseController.getUser(userToTest);
+				Assert.assertEquals(newLastName, user.getLastName());
+			}
+
+		}
+
