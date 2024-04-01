090510f674eaab475adf7b1df25838f7354d9744
WVALVERDE001 <wvalverde001@vplincsci047.ad.csbsju.edu>
2024-03-31 20:36:45 -0500

    Created a AddUserTest

---
+++ test/regression/AddUserTest.java
@@ -0,0 +1,36 @@
+package regression;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cmc.backend.AccountController;
+import cmc.backend.DatabaseController;
+import cmc.backend.User;
+import cmc.frontend.UserInteraction;
+import junit.framework.Assert;
+
+public class AddUserTest {
+	private static String username = "Vijay_Dr.V_TheGoats123!!!???7771234567890123456789009876543210987654321";
+	private static String passcode = "kohls";
+
+	@Before
+	public void setUp() throws Exception {
+		User newUser = new User(username, passcode, 'u',"", "", 'y');
+		DatabaseController.addUser(newUser);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		DatabaseController.removeUser(username);
+	}
+
+	@Test
+	public void test() {
+		
+		Assert.assertEquals(true, UserInteraction.login(username, passcode));
+	}
+
+}
