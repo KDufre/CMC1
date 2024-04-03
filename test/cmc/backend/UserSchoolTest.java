cdc4ec151deb9fc45a3ba4a33f3cf5743044fb5b
WVALVERDE001 <wvalverde001@vplincsci025.ad.csbsju.edu>
2024-04-02 23:23:04 -0500

    nothing happened

---
+++ test/cmc/backend/UserSchoolTest.java
@@ -0,0 +1,28 @@
+package cmc.backend;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserSchoolTest {
+	private String University;
+	private Date timestamp;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testUserSchool() {
+		
+		
+	}
+
+
+}
