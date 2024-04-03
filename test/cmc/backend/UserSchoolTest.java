a1934ffc7eb101f792e16835ee821bb097247808
WVALVERDE001 <wvalverde001@vplincsci069.ad.csbsju.edu>
2024-04-03 16:47:46 -0500

    Created a userschoolTest

--- test/cmc/backend/UserSchoolTest.java
+++ test/cmc/backend/UserSchoolTest.java
@@ -6,21 +6,33 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class UserSchoolTest {
-	private String University;
-	private Date timestamp;
+	private String University = "William";
+	private Date timestamp = new Date(234);
+	private UserSchool userschool;
 
 	@Before
 	public void setUp() throws Exception {
+		userschool = new UserSchool(University, timestamp);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
 	public void testUserSchool() {
+		Assert.assertEquals("William", userschool.getUniversity());
+		userschool.setUniversity("Valverde");
+		Assert.assertEquals("Valverde",userschool.getUniversity());
 		
+		Date newdate = new Date(123);
+		Assert.assertEquals(timestamp, userschool.getTimestamp());
+		userschool.setTimestamp(newdate);
+		Assert.assertEquals(newdate, userschool.getTimestamp());
 		
 	}
 
