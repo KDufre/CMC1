1a6c222d83a0e1e489d2d80d3fd471fa9e6ae53b
Cormac O'Connor <coconnor001@csbsju.edu>
2024-04-05 16:43:21 -0500

    Added testSavedSchools subtest to SearchController JUnit Test

--- test/cmc/backend/SearchControllerTest.java
+++ test/cmc/backend/SearchControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import cmc.backend.entities.University;
+import cmc.backend.entities.UserSchool;
 import junit.framework.Assert;
 
 public class SearchControllerTest {
@@ -16,14 +17,15 @@ public class SearchControllerTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		//AccountController.addUser("5thPrez", "Monroedoctrine", "James", "Monroe", false);
+		AccountController.addUser("5thPrez", "Monroedoctrine", "James", "Monroe", false);
 		UniversityController.addUniversity("JamesMonroeUniversity", "Virginia", "MonroeHall", "Public", 1758, 0.0, 900, 1200, 71831, 40, 10000, 60, 90, 8, 7, 5, 0, "google.com");
-		
+		UniversityController.saveSchool("5thPrez", "JamesMonroeUniversity");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//AccountController.removeUser("5thPrez");
+		UniversityController.removeSchool("5thPrez", "JamesMonroeUniversity");
+		AccountController.removeUser("5thPrez");
 		UniversityController.deleteUniversity("JamesMonroeUniversity");
 	}
 
@@ -32,12 +34,13 @@ public class SearchControllerTest {
 		List<University> list = SearchController.search("JamesMonroeUniversity", "Virginia", "MonroeHall", "Public", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "P", "O", "L", "I", "C");
 		Assert.assertEquals(list.size(), 0);
 	}
-/*
+
 	@Test
 	public void testGetSavedSchools() {
-		fail("Not yet implemented");
+		List<UserSchool> list2 = SearchController.getSavedSchools("5thPrez");
+		Assert.assertEquals(list2.size(), 1);
 	}
-
+/*
 	@Test
 	public void testGetAllUsers() {
 		fail("Not yet implemented");
