8cdd0f0e27ff51f5c783fd2f97a378d3d2cb7b72
WVALVERDE001 <wvalverde001@vplincsci038.ad.csbsju.edu>
2024-04-06 16:32:07 -0500

    Wrote some javadoc comments

--- src/cmc/backend/AccountController.java
+++ src/cmc/backend/AccountController.java
@@ -5,6 +5,12 @@ import java.util.List;
 import cmc.backend.entities.User;
 
 public class AccountController {
+	/**
+	 * This method allows the user to login to the CMC.
+	 * @param username is required to login
+	 * @param password is also required to login
+	 * @return User that logs in, if false it returns null
+	 */
 	public static User login(String username, String password) {
 		User theUser = DatabaseController.getUser(username);
 
@@ -19,6 +25,15 @@ public class AccountController {
 
 	// this ADMIN ONLY method attempts to add a user to the database with the
 	// provided details
+	/**
+	 * This method adds a User with their important information
+	 * @param username is needed for login
+	 * @param password is needed for login
+	 * @param firstName is needed for the user identity
+	 * @param lastName is the users last name
+	 * @param isAdmin true if user is an admin, false if user is not an admin
+	 * @return boolean true if the User was added successfully, false if the user was already added
+	 */
 	public static boolean addUser(String username, String password,
 			String firstName, String lastName, boolean isAdmin) {
 		char type = (isAdmin ? 'a' : 'u');
@@ -30,33 +45,64 @@ public class AccountController {
 
 	// this ADMIN ONLY method attempts to remove a user from the database
 	// based on the provided username
+	/**
+	 * This method removes a user from the database
+	 * @param username that is being removed from the system
+	 * @return boolean if successfully removed from the database
+	 */
 	public static boolean removeUser(String username) {
 		return DatabaseController.removeUser(username);
 	}
 
 	//changes password
+	/**
+	 * This method allows the user to be able to edit their password
+	 * @param username of user who wants to change their password
+	 * @param newPassword the user enters their desired new password
+	 * @return boolean true if changing the password was successfull
+	 */
 	public static boolean editPassword(String username, String newPassword) {
 		User user = DatabaseController.getUser(username);
 		User userUpdate = new User(username, newPassword, user.type, user.getFirstName(), user.getLastName(), user.activated);
 		return DatabaseController.editUser(userUpdate);
 	}
 	//changes firstName
+	/**
+	 * This method allows the user to change their first name
+	 * @param username the users username to know who is changing it
+	 * @param newFirstName user enters their new name is updated
+	 * @return boolean true if the update was successfull
+	 */
 	public static boolean editFirstName(String username, String newFirstName) {
 		User user = DatabaseController.getUser(username);
 		User userUpdate = new User(username, user.getPassword(), user.type, newFirstName, user.getLastName(), user.activated);
 		return DatabaseController.editUser(userUpdate);
 	}
 	//changes lastName
+	/**
+	 * This method allows the user to change their last name
+	 * @param username the users username to know who is changing it
+	 * @param newFirstName user enters their new lastname is updated
+	 * @return boolean true if the update was successfull
+	 */
 	public static boolean editLastName(String username, String newLastName) {
 		User user = DatabaseController.getUser(username);
 		User userUpdate = new User(username, user.getPassword(), user.type, user.getFirstName(), newLastName, user.activated);
 		return DatabaseController.editUser(userUpdate);
 	}
-
+	/**
+	 * This method allows admin to deactivate the user
+	 * @param username of the user that is being deactivated
+	 * @return boolean true if user is now deactivated
+	 */
 	public static boolean deactivateUser(String username) {
 		return DatabaseController.deactivateUser(username);
 	}
-
+	/**
+	 * This method allows admin to activate the user
+	 * @param username of the user that is being activated
+	 * @return boolean true if user is now activated
+	 */
 	public static boolean activateUser(String username) {
 		return DatabaseController.activateUser(username);
 	}
--- src/cmc/backend/UniversityController.java
+++ src/cmc/backend/UniversityController.java
@@ -8,6 +8,13 @@ import cmc.backend.entities.UserSchool;
 public class UniversityController {
 	// this REGULAR USER ONLY method attempts to add the provided school
 	// to the list of saved schools for the provided username
+	/**
+	 * This method attemps to add the provided school to the list of saved schools
+	 * for the provided username
+	 * @param user get the user to say they can save a school
+	 * @param school that is chosen is being saved
+	 * @return boolean true if the school was successfuly saved
+	 */
 	public static boolean saveSchool(String user, String school) {
 		List<UserSchool> schools = SearchController.getSavedSchools(user); 
 		if(schools!=null) {
@@ -20,17 +27,36 @@ public class UniversityController {
 		}
 		return DatabaseController.saveSchool(user, school);
 	}
-
+	/**
+	 * This method removes a school from the user
+	 * @param user is chosen to be able to remove school
+	 * @param school is removed
+	 * @return boolean true if school was removed succesfully
+	 */
 	public static boolean removeSchool(String user, String school) {
 		return DatabaseController.removeSchool(user, school);
 	}
-
+	/**
+	 * This method adds a university to the system
+	 * @param uni is the uni that is being added
+	 * @return boolean false if university was already added to the system before.
+	 */
 	public static boolean addUniversity(University uni) {
 		return DatabaseController.addUniversity(uni);
 	}
+	/**
+	 * This method deletes the university from the system
+	 * @param school chosen is deleted from the system
+	 * @return boolean true if uni can be removed from the system
+	 */
 	public static boolean deleteUniversity(String school) {
 		return DatabaseController.deleteUniversity(school);
 	}
+	/**
+	 * This method allows the admin to edit the University
+	 * @param uni chosen can be edited
+	 * @return boolean false if university not in the database
+	 */
 	public static boolean editUniversity(University uni) {
 		return DatabaseController.editUniversity(uni);
 	}
