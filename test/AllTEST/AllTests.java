2946f478c115f0c4eab6132529483136a001e163
KDUFRESNE001 <kdufresne001@csbsju.edu>
2024-04-08 22:49:37 -0500

    added some tests to Alltests

--- test/AllTEST/AllTests.java
+++ test/AllTEST/AllTests.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import cmc.backend.AccountControllerTest;
 import cmc.backend.DatabaseControllerTest;
 import cmc.backend.SearchControllerTest;
+import cmc.backend.UniversityControllerTest;
 import cmc.backend.UniversityTest;
 import cmc.backend.UserSchoolTest;
 import cmc.backend.UserTest;
@@ -16,7 +18,7 @@ import system.*;
 @SuiteClasses({ DatabaseControllerTest.class, SearchControllerTest.class, UniversityTest.class, UserSchoolTest.class,
 		UserTest.class, DeactivatedUserTest.class, RemoveUserTest.class, SearchWithoutStateTest.class, ActivateUserTest.class
 		, AddSavedSchoolsTest.class, AddUniversityTest.class, AddUserTest.class, EditUserTest.class, LoggingUserTest.class
-		, RemoveUniversityTest.class})
+		, RemoveUniversityTest.class, UniversityTest.class, AccountControllerTest.class, UniversityControllerTest.class})
 public class AllTests {
 
 }
