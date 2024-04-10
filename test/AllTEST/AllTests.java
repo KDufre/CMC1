113eacf7082019447db95a135f005f49a0219264
KDUFRESNE001 <kdufresne001@csbsju.edu>
2024-04-09 21:33:40 -0500

    updated AllTests

--- test/AllTEST/AllTests.java
+++ test/AllTEST/AllTests.java
@@ -19,7 +19,8 @@ import system.*;
 		UserTest.class, DeactivatedUserTest.class, RemoveUserTest.class, SearchWithoutStateTest.class, ActivateUserTest.class
 		, AddSavedSchoolsTest.class, AddUniversityTest.class, AddUserTest.class, EditUserTest.class, LoggingUserTest.class
 		, RemoveUniversityTest.class, UniversityTest.class, AccountControllerTest.class, UniversityControllerTest.class
-		, ViewSavedSchoolsNoneTest.class, FiveSimilarSchoolsTest.class})
+		, ViewSavedSchoolsNoneTest.class, FiveSimilarSchoolsTest.class, DeactivateUserTest.class
+		, EditUniversityTest.class})
 public class AllTests {
 
 }
